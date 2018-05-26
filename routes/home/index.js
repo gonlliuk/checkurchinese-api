99336702c79afe6323016d6c9c4080434693abfd
Stanislav Voitov <svoitov@exadel.com>
2018-05-26 16:27:08 +0500

    fix routes

--- routes/home/index.js
+++ routes/home/index.js
@@ -7,14 +7,14 @@ module.exports = ({ authorize, models }) => {
     const routes = new Controller({ models });
 
     // Routes configuration
-    router.get('/', routes.getHome());
+    router.get('/', routes.getHome);
 
     // Check all routes for valid token
     router.use(authorize);
 
-    router.put('/', Validator.createHome(), routes.createHome());
+    router.put('/', Validator.createHome(), routes.createHome);
     router.patch('/:homeId', Validator.patchHome(), routes.patchHome);
-    router.delete('/:homeId', Validator.deleteHome(), routes.deleteHome());
+    router.delete('/:homeId', Validator.deleteHome(), routes.deleteHome);
 
     return router;
 };
