2f535e3bb8c4a25a0cb56b23505ee86f3b3b959c
Stanislav Voitov <svoitov@exadel.com>
2018-06-04 13:56:54 +0500

    add user and password to mongodb options

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -6,12 +6,16 @@ const config = require('../config');
 
 const CONNECTION_STRING = `${config.get('MONGODB:HOST')}/${config.get('MONGODB:DB')}`;
 
+const OPTIONS = config.get('MONGODB:OPTIONS');
+OPTIONS.user = config.get('MONGODB:USER');
+OPTIONS.pass = config.get('MONGODB:PASS');
+
 require('mongoose-double')(mongoose);
 mongoose.Promise = require('bluebird');
 
 if (process.env.NODE_ENV !== 'test') {
     mongoose
-        .connect(CONNECTION_STRING, config.get('MONGODB:OPTIONS'))
+        .connect(CONNECTION_STRING, OPTIONS)
         .then(() => log(`:: MONGOOSE CONNECTED > ${CONNECTION_STRING}`, 'blue'))
         .catch(error => inspect(error));
 }
